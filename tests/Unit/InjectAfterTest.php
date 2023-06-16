5c23dfb580eef0e18f664713d226cb7435221b33
gneb <gneb@outlook.com>
2023-06-16 22:43:08 +0400

    added test case if item was added to array

--- tests/Unit/InjectAfterTest.php
+++ tests/Unit/InjectAfterTest.php
@@ -2,12 +2,16 @@
 
 namespace Tests\Unit;
 
+use App;
 use PHPUnit\Framework\TestCase;
 
 class InjectAfterTEst extends TestCase
 {
-    public function it_inserts_end_of_array(): void 
+    /** @test*/
+    public function it_inserts_to_array(): void 
     {
-        
+        $res = injectAfter(["foo" => 3, "bar" => 1, "bob" => 5, "gog" => 6], 'bbb', 'aaa', 7);
+        $excepted = 5;
+        $this->assertEquals($excepted, count($res));
     }
 }
