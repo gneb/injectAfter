8c00ed8f03ceb896fbc5d4e7fd7ad72e53065263
gneb <gneb@outlook.com>
2023-06-16 18:29:09 +0400

    finished injectAfter in php

--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 
 require_once "./vendor/autoload.php";
 
-injectAfter([], '', '', '');
+$res = injectAfter(["foo" => 3, "bar" => 1, "bob" => 5, "gog" => 6], 'gog', 'foo', 7);
+
+print_r($res);
--- src/injectAfter.php
+++ src/injectAfter.php
@@ -1,6 +1,35 @@
 <?php
 
-function injectAfter(array $array, string $afterKey, string $newKey, mixed $newValue)
-{
-    echo 1;
+if( ! function_exists('injectAfter') ) {
+    function injectAfter(array $array, string $afterKey, string $newKey, mixed $newValue)
+    {
+        $issetAftrerKey = isset($array[$afterKey]);
+        $issetNewKey = isset($array[$newKey]);
+
+        // if we have new key in array but dont have after key at same time
+        if(!$issetAftrerKey && $issetNewKey) {
+            throw new Exception('Unhandled');
+        }
+
+        // insert end of array
+        if(!$issetAftrerKey) {
+            $array[$newKey] = $newValue;
+            return $array;
+        }
+
+        // remove current and call recursive to add in new position
+        if($issetNewKey) {
+            unset($array[$newKey]);
+            injectAfter($array, $afterKey, $newKey, $newValue);
+        }
+
+        // slive array and insert end of first part
+        $indexToSlice = array_search($afterKey,array_keys($array));
+        $leftPart = array_slice($array, 0, $indexToSlice + 1);
+        $rightPart = array_slice($array, $indexToSlice + 1, count($array));
+        $leftPart[$newKey] = $newValue;
+        return array_merge($leftPart, $rightPart);
+    
+    }
 }
+
