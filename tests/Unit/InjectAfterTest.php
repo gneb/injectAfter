e029cdec4e45be7ca9a9a0c3dd3710b9bb73d648
gneb <gneb@outlook.com>
2023-06-16 19:01:14 +0400

    added phpunit config file and directory

---
+++ tests/Unit/InjectAfterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class InjectAfterTEst extends TestCase
+{
+    public function it_inserts_end_of_array(): void 
+    {
+        
+    }
+}
